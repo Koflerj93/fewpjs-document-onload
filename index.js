b3bb201160be235cd5f1ab06d88b0809a0aeb971
Koflerj93 <koflerj93@gmail.com>
2020-11-17 22:00:45 -0500

    commit tree 505a36644afb8700f4f97d965304c6ec055985b3

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+// This appears second, tells us the DOm has loaded fully
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//   }); 
+
+
+// this appears in console first since it is outside of the DCL 
+// console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+// ); 
+
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    updateText()
+}); 
+
+function updateText() {
+    const text = document.querySelector("#text");
+    text.innerHTML = "This is really cool!";
+}
